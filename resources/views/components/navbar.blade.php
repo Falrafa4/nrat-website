4176949a57c57a99252c8c335167314a0f83043a
Falrafa4 <rafaasad9@gmail.com>
2026-06-13 12:29:59 +0700

    feat: add background image to landing page, enhance navbar structure, and create reusable button component

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,3 +1,9 @@
 <nav>
-    <h1>NRATrainz</h1>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">My Content</a></li>
+        <li><a href="/contact">Ketentuan</a></li>
+        <li><a href="/contact">Kontak</a></li>
+        <li><a href="/contact">About</a></li>
+    </ul>
 </nav>
--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="h-screen flex items-center justify-center font-header">
-        <h1 class="text-4xl font-bold">NRATrainz</h1>
-    </div>
+    <header class="relative h-screen overflow-hidden px-4 flex flex-col items-center justify-center font-header text-background">
+        <div class="absolute inset-0 scale-110 bg-cover bg-center blur-sm z-5"
+            style="background-image: url('{{ asset('images/bg-navbar.png') }}');"></div>
+        <h1 class="relative z-10 text-8xl font-bold">NRATrainz</h1>
+        <p class="relative z-10 mt-4 text-xl"> Penyedia addons “kecil-kecilan” untuk Trainz Simulator</p>
+        <x-button>Lihat Koleksi</x-button>
+    </header>
 @endsection
