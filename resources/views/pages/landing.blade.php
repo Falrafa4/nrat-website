c40a194d37c95a1ba574c1448536a67c5fe8e598
Falrafa4 <rafaasad9@gmail.com>
2026-06-13 20:39:52 +0700

    feat: replace navbar background image with new webp format and add additional images

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <header class="relative h-screen overflow-hidden px-4 flex flex-col items-center justify-center font-title text-background">
         <div class="absolute inset-0 scale-110 bg-cover bg-center blur-sm z-5 bg-fixed"
-            style="background-image: url('{{ asset('images/bg-navbar.png') }}');"></div>
+            style="background-image: url('{{ asset('images/bg-navbar.webp') }}');"></div>
         <h1 class="relative z-10 text-8xl font-bold">NRATrainz</h1>
         <p class="relative z-10 my-4 text-xl"> Penyedia addons “kecil-kecilan” untuk Trainz Simulator</p>
         <x-button>Lihat Koleksi</x-button>
