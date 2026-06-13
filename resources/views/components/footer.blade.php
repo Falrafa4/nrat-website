b376650f509179515f9b7217e7f94f2d10dbf902
Falrafa4 <rafaasad9@gmail.com>
2026-06-13 20:35:06 +0700

    feat: enhance footer component with styling, social icons, and related links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,21 @@
-<footer>
-    <h1>NRATrainz</h1>
+<footer class="bg-primary-950 text-background flex flex-col md:flex-row justify-between items-start px-32 py-16">
+    <div class="footer-left flex flex-col">
+        <h1 class="text-3xl font-bold">NRATrainz</h1>
+        <div class="social-icons space-x-2 my-2">
+            <a class="text-3xl" href="#"><i class="fab fa-facebook"></i></a>
+            <a class="text-3xl" href="#"><i class="fab fa-youtube"></i></a>
+            <a class="text-3xl" href="#"><i class="fab fa-instagram"></i></a>
+            <a class="text-3xl" href="#"><i class="fab fa-whatsapp"></i></a>
+        </div>
+        <p>&copy; Copyright 2026 NRATrainz. All rights reserved.</p>
+    </div>
+    <div class="footer-right flex flex-col gap-2">
+        <h2 class="text-xl font-bold">Link Terkait</h2>
+        <div class="footer-links flex flex-col">
+            <a class="text-md font-light text-background/80 hover:text-background transition" href="#">Home</a>
+            <a class="text-md font-light text-background/80 hover:text-background transition" href="#">Addons Terbaru</a>
+            <a class="text-md font-light text-background/80 hover:text-background transition" href="#">Addons Lainnya</a>
+            <a class="text-md font-light text-background/80 hover:text-background transition" href="#">Hubungi Kami</a>
+        </div>
+    </div>
 </footer>
